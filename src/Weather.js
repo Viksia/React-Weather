6f28af4d96f411b4514d1af585ae174c2ae6fa33
Viksia <105889022+Viksia@users.noreply.github.com>
2022-09-11 20:35:45 +0200

    Added basic CSS

--- src/Weather.js
+++ src/Weather.js
@@ -13,10 +13,15 @@ export default function Weather() {
                 type="search"
                 placeholder="Enter a city.."
                 className="form-control"
+                autoFocus="on"
               />
             </div>
             <div className="col-3">
-              <input type="Submit" value="Search" className="btn btn-primary" />
+              <input
+                type="Submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
         </form>
@@ -26,13 +31,19 @@ export default function Weather() {
           <li>Wednesday 07:00</li>
           <li>Mostly Cloudy</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
-              alt="cloudy"
-            />
-            6 °C
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+                alt="cloudy"
+                className="float-left"
+              />
+              <div className="float-left">
+                <span className="temperature">6</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
           </div>
           <div className="col-6">
             <ul>
@@ -43,7 +54,8 @@ export default function Weather() {
           </div>
         </div>
       </div>
-      <small>
+
+      <footer>
         <a
           href="https://github.com/Viksia/My-project"
           target="_blank"
@@ -52,7 +64,7 @@ export default function Weather() {
           Open-source code
         </a>{" "}
         by Viksy
-      </small>
+      </footer>
     </div>
   );
 }
