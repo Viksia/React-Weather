60fecc4e93b28ba2296d359ab06c84ff4c3bc45f
Viksia <105889022+Viksia@users.noreply.github.com>
2022-09-13 00:37:15 +0200

    Added Date component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 export default function App() {
   return (
     <div className="App">
-      <Weather defaultCity="Kyiv" />
+      <Weather defaultCity="London" />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function Weather(props) {
@@ -12,7 +13,7 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      date: "Wednesday, 07:00",
+      date: new Date(response.data.dt * 1000),
       iconUrl: "https://ssl.gstatic.com/onebox/weather/48/sunny_s_cloudy.png",
       wind: response.data.wind.speed,
       city: response.data.name,
@@ -45,7 +46,9 @@ export default function Weather(props) {
 
           <h1>{weatherData.city}</h1>
           <ul>
-            <li>{weatherData.date}</li>
+            <li>
+              <FormattedDate date={weatherData.date} />
+            </li>
             <li className="text-capitalize">{weatherData.description}</li>
           </ul>
           <div className="row mt-3">
